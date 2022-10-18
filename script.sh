b71ec8138fa7ab9b1f9abba4a91baa75256c9b65
Divyansh Raina <divicoolman729@gmail.com>
2022-10-18 23:06:03 +0530

    Update

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
 sudo yum install libXcomposite libXcursor libXi libXtst libXrandr alsa-lib mesa-libEGL libXdamage mesa-libGL libXScrnSaver
-
+sudo yum install perl-Digest-SHA -y
+cd /home/$USER/Downloads
+wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh 
+pip install pyspark
