494a33b7b2ce8f3f1ed882b594fdbb2417a0f9a2
Divyansh Raina <divicoolman729@gmail.com>
2022-11-16 22:03:51 +0530

    Update

--- main.py
+++ main.py
@@ -34,16 +34,16 @@ if __name__ == "__main__":
     # count word split 
     wordsplitRDD = splitRDD.map(lambda x: (x, 1))
     wordCounts = wordsplitRDD.reduceByKey(lambda x,y : x+y)
-    print(wordCounts.collect())
+    
     
 
     # find stop words 
-    stopwordcounts20 = wordCount(splitRDD).sortBy(lambda x: x[1], ascending=False).take(20) 
-    stop_words = [i[0] for i in stopwordcounts20]
-
+    stopwordcounts25 = wordCount(splitRDD).sortBy(lambda x: x[1], ascending=False).take(25) 
+    stop_words = [i[0] for i in stopwordcounts25]
+    # print(stop_words)
      # convert to lowercase from splitRDD and remove stopword from file 
     # link: https://gist.github.com/sebleier/554280 
-    #  stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 
+    # stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 
     #  'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 
     #  'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them',
     #   'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     #   'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 
     #   'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 
     #   'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 
-    #           'very', 'can', 'will', 'just', 'don', 'should', 'now']
+    #           'very', 'can', 'will', 'just', 'don', 'should', 'now','']
 
     splitRDD_filter =  splitRDD.filter(lambda word: word.lower() not in stop_words )
 
@@ -63,10 +63,10 @@ if __name__ == "__main__":
     reducedRDD = PairRDD_tuple.reduceByKey(lambda x,y: x + y)
     
     # show 10 word 
-    print(reducedRDD.take(10))
+    #print(reducedRDD.take(10))
 
     # Hoan doi key va value 
     swappedRDD = reducedRDD.map(lambda word: (word[1], word[0]))
     resultRDD = swappedRDD.sortByKey(ascending=False)
 
-    print(resultRDD.take(10))
+    print(resultRDD.take(50))
